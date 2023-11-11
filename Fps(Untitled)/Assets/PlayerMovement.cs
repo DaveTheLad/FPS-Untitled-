9cdb967e9ca0262ec17673d331ecf4493dbe18e7
DawidKopecki <davidisheree@hotmail.com>
2023-11-11 17:22:03 +0000

    Jumping + Comments
    (Jump) needs fixing as it is enabled the whole time

--- Fps(Untitled)/Assets/PlayerMovement.cs
+++ Fps(Untitled)/Assets/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
 
     public float speed = 12f;
     public float gravity = -9.80f;
+    public float JumpHeight = 3f;
 
     public Transform groundCheck;
     public float groundDistance = 0.4f;
@@ -16,27 +17,39 @@ public class PlayerMovement : MonoBehaviour
     Vector3 velocity;
     bool isGrounded;
 
-  
-
     // Update is called once per frame
     void Update()
     {
+        // Check if the player is grounded using a sphere check at the specified position
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        if(isGrounded && velocity.y <0)
+        // If the player is grounded and falling, reset the vertical velocity
+        if (isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
         }
 
+        // Get horizontal and vertical input for player movement
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.right * x + transform.transform.forward * z;
+        // Calculate the movement direction based on the input and player's orientation
+        Vector3 move = transform.right * x + transform.forward * z;
 
+        // Move the player using CharacterController with speed and time-based deltaTime
         controller.Move(move * speed * Time.deltaTime);
 
+        // Check for the jump input and whether the player is grounded
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            // Apply the jump formula to calculate the initial jump velocity
+            velocity.y = Mathf.Sqrt(JumpHeight * -2f * gravity);
+        }
+
+        // Apply gravity to the player's vertical velocity over time
         velocity.y += gravity * Time.deltaTime;
 
+        // Move the player vertically using CharacterController with time-based deltaTime
         controller.Move(velocity * Time.deltaTime);
     }
 }
--- Fps(Untitled)/Assets/mouseLook.cs
+++ Fps(Untitled)/Assets/mouseLook.cs
@@ -4,29 +4,39 @@ using UnityEngine;
 
 public class mouseLook : MonoBehaviour
 {
-
+    // Sensitivity of the mouse movement
     public float mouseSensitivity = 100f;
 
+    // Reference to the player's body (used for horizontal rotation)
     public Transform playerBody;
 
+    // Initial rotation around the X-axis
     float xRotation = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Lock the cursor to the center of the screen
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Get mouse input for X and Y axis
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
+        // Adjust the rotation around the X-axis based on the mouseY input
         xRotation -= mouseY;
+
+        // Clamp the vertical rotation to prevent over-rotation
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
+        // Apply the rotation to the camera around the local X-axis
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        // Rotate the player's body horizontally based on the mouseX input
         playerBody.Rotate(Vector3.up * mouseX);
     }
 }
