9db5da8b863c0facf74d2ffadd22886b2899a54b
DawidKopecki <davidisheree@hotmail.com>
2023-11-19 18:55:25 +0000

    Added basic shooting script
    also Added target script on wooden crate which could be reused later for enemy
    + basic cross hair (subject to change)

---
+++ Assets/Target.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public float health = 50f;
+
+    public void TakeDame(float amount)
+    {
+        health -= amount;
+        if (health <= 0f)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/pistolScript.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class pistolScript : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+
+    public Camera fpsCam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if(target != null)
+            {
+                target.TakeDame(damage);
+            }
+        }
+    }
+}
